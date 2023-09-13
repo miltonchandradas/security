e38921035949e8edbe713d95bb758b0a175978a5
Milton Chandradas <milton.chandradas@sap.com>
2023-09-13 00:38:13 -0400

    headers...

--- srv/demo-service.js
+++ srv/demo-service.js
@@ -3,9 +3,15 @@ const cds = require("@sap/cds");
 module.exports = async (srv) => {
   const { Employees, Departments } = srv.entities;
 
+  srv.on("*", async (req, next) => {
+    req.headers["id"] = req.user?.id;
+    req.headers["email"] = req.user?.attributes?.email;
+    next();
+  });
+
   srv.on("READ", Employees, async (req, next) => {
-    console.log("Logged in user: ", req.user?.id);
-    console.log("Email: ", req.user?.attributes?.email);
+    console.log("Logged in user: ", req.headers?.id);
+    console.log("Email: ", req.headers?.email);
     return await next();
   });
 };
