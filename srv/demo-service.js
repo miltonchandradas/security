36df87155149093871e1e1019b4300b46a01143d
Milton Chandradas <milton.chandradas@sap.com>
2023-09-12 23:52:02 -0400

    logged in user...

--- srv/demo-service.js
+++ srv/demo-service.js
@@ -1,6 +1,11 @@
 const cds = require("@sap/cds");
-// cds.env.requires.auth.users["*"] = false;
+cds.env.requires.auth.users["*"] = false;
 
 module.exports = async (srv) => {
   const { Employees, Departments } = srv.entities;
+
+  srv.on("READ", Employees, async (req, next) => {
+    console.log("Logged in user: ", req.user?.id);
+    return await next();
+  });
 };
