fdf2d27c6638e2b3cb7fab1aa701ec1c38929925
Milton Chandradas <milton.chandradas@sap.com>
2023-09-13 00:21:00 -0400

    attributes...

--- srv/demo-service.js
+++ srv/demo-service.js
@@ -1,11 +1,11 @@
 const cds = require("@sap/cds");
-cds.env.requires.auth.users["*"] = false;
 
 module.exports = async (srv) => {
   const { Employees, Departments } = srv.entities;
 
   srv.on("READ", Employees, async (req, next) => {
     console.log("Logged in user: ", req.user?.id);
+    console.log("Email: ", req.user?.attributes?.email);
     return await next();
   });
 };
