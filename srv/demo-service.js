013f60224a4f589ab4976f681cab96a42e78f0f8
Milton Chandradas <milton.chandradas@sap.com>
2023-09-16 06:59:23 -0400

    jwt token...

--- srv/demo-service.js
+++ srv/demo-service.js
@@ -1,11 +1,20 @@
 const cds = require("@sap/cds");
+const jwtDecode = require("jwt-decode");
 
 module.exports = async (srv) => {
   const { Employees, Departments } = srv.entities;
 
   srv.before("*", async (req) => {
-    req.headers["id"] = req.user?.id;
-    req.headers["email"] = req.user?.attributes?.email;
+    if (process.env.ENVIRONMENT === "development") {
+      req.headers["id"] = req.user?.id;
+      req.headers["email"] = req.user?.attributes?.email;
+    } else if (process.env.ENVIRONMENT === "production") {
+      // TODO: Read values from JWT token and store in headers...
+      let jwt = req.headers.authorization;
+      console.log("JWT Token: ", jwt);
+      let jwtDecoded = jwtDecode(jwt);
+      console.log("Decoded JWT Token: ", jwtDecoded);
+    }
   });
 
   srv.on("READ", Employees, async (req, next) => {
